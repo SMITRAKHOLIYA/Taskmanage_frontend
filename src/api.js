fb7bb999cfe0903b83e489041e2f558f7fe2e287
smit <smitrakholiya68@gmail.com>
2025-12-22 18:29:38 +0530

    remove media from task dea

--- src/api.js
+++ src/api.js
@@ -1,3 +1,32 @@
+// import axios from 'axios';
+
+// const API_URL = import.meta.env.VITE_API_URL || '/api';
+
+// const api = axios.create({
+//     baseURL: API_URL,
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+// });
+
+// export const MEDIA_URL = API_URL.replace('/api', '');
+
+// api.interceptors.request.use(
+//     (config) => {
+//         const user = JSON.parse(localStorage.getItem('user'));
+//         if (user && user.token) {
+//             config.headers.Authorization = `Bearer ${user.token}`;
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         return Promise.reject(error);
+//     }
+// );
+
+// export default api;
+
+
 import axios from "axios";
 
 // IMPORTANT: api.php must be in baseURL
@@ -28,5 +57,5 @@ api.interceptors.request.use(
     },
     (error) => Promise.reject(error)
 );
-
+export const MEDIA_URL = API_URL.replace('/api', '');
 export default api;
