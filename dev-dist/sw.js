449c183bea743b8a32096e048a775f689d8a7466
smit <smitrakholiya68@gmail.com>
2025-12-23 11:36:36 +0530

    fix corss error

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.nfq9s16qmk8"
+    "revision": "0.60p36hj78ag"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/api.js
+++ src/api.js
@@ -1,61 +1,39 @@
-// import axios from 'axios';
-
-// const API_URL = import.meta.env.VITE_API_URL || '/api';
-
-// const api = axios.create({
-//     baseURL: API_URL,
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-// });
-
-// export const MEDIA_URL = API_URL.replace('/api', '');
-
-// api.interceptors.request.use(
-//     (config) => {
-//         const user = JSON.parse(localStorage.getItem('user'));
-//         if (user && user.token) {
-//             config.headers.Authorization = `Bearer ${user.token}`;
-//         }
-//         return config;
-//     },
-//     (error) => {
-//         return Promise.reject(error);
-//     }
-// );
-
-// export default api;
-
-
 import axios from "axios";
 
+
 // IMPORTANT: api.php must be in baseURL
-const API_URL = import.meta.env.VITE_API_URL || "https://taskmanage.iceiy.com/api.php";
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api.php";
 
 const api = axios.create({
     baseURL: API_URL,
     // DO NOT set default Content-Type
     // DO NOT use withCredentials
 });
-
+export const MEDIA_URL = API_URL.replace('/api', '');
 // Attach token ONLY when needed (not for login/register)
 api.interceptors.request.use(
     (config) => {
-        const user = JSON.parse(localStorage.getItem("user"));
+        const storedUser = localStorage.getItem("user");
+        const user = storedUser ? JSON.parse(storedUser) : null;
+
+        // Ensure URL is relative to baseURL if it starts with /
+        if (config.url.startsWith('/')) {
+            config.url = config.url.substring(1);
+        }
 
         // Skip token for auth endpoints
         if (
             user &&
             user.token &&
-            !config.url.includes("/auth/login") &&
-            !config.url.includes("/auth/register")
+            !config.url.includes("auth/login") &&
+            !config.url.includes("auth/register")
         ) {
-            config.headers.Authorization = `Bearer ${user.token}`;
+            config.headers.Authorization = `Bearer ${user.token} `;
         }
 
         return config;
     },
     (error) => Promise.reject(error)
 );
-export const MEDIA_URL = API_URL.replace('/api', '');
+
 export default api;
